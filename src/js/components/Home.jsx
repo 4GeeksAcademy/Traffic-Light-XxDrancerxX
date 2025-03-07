3a78bc1b2428ed1b0ae2f43bfea4c3e2107a952e
XxDrancerxX <idmdiazmora98@gmail.com>
2025-03-07 03:38:29 +0000

    done

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	// {First solution}
+// 	const [selectedColor, setSelectedColor] = useState("red");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+// 	return (
+
+// 		<div className="traffic-light">
+// 			<div onClick={() => setSelectedColor("red")} className={`light red ${selectedColor === "red" ? "glow" : ""}`}></div>
+// 			<div onClick={() => setSelectedColor("yellow")} className={`light yellow ${selectedColor === "yellow" ? "glow" : ""}`}></div>
+// 			<div onClick={() => setSelectedColor("green")} className={`light green ${selectedColor === "green" ? "glow" : ""}`}></div>
+// 		</div>
+// 	)
+// };
+
+        //   {Second Solution}:
+
+	const [Light, setLight] = useState("null");
+
+	return (
+		<div className="container d-flex flex-column align-items-center bg-secondary min-vh-100 ">
+
+			<div className="black-container bg-dark border"></div>
+			<div className="traffic-light bg-dark border">
+				<div onClick={() => { setLight("red") }}
+					className={`light bg-danger rounded-circle border ${Light === "red" ? "active-light" : ""}`}>
+				</div>
+				<div onClick={() => { setLight("yellow") }}
+					className={`light bg-warning rounded-circle border ${Light === "yellow" ? "active-light" : ""}`}>
+				</div>
+
+				<div onClick={() => { setLight("green") }}
+				 className={`light bg-success rounded-circle border ${Light === "green" ? "active-light" : ""}`}>
+
+				</div>
+
+			</div>
+
+
+
 		</div>
 	);
 };
